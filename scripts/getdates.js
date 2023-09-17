a8c3a0abc6c4c157110d6e1e8052f7c4bf7a7e2f
Kate DeForest <katedeforest02@gmail.com>
2023-09-16 18:21:27 -0600

    CSS edits and add JS file

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+// Get Current Year
+const options = {
+  year: "numeric",
+};
+document.querySelector("#currentYear").innerHTML =
+  new Date().toLocaleDateString("en-US", options);
+
+// Get Last Modified Date
+document.querySelector("#lastModified").innerHTML = document.lastModified;
