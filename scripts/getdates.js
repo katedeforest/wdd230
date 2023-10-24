9493d6f0ffa7bf4c3268f21f0dc62691152ed5da
Kate DeForest <katedeforest02@gmail.com>
2023-10-24 15:36:19 -0600

    lazy load update

--- scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-// Get Current Year
-const options = {
-  year: "numeric",
-};
-document.querySelector("#currentYear").innerHTML =
-  new Date().toLocaleDateString("en-US", options);
-
-// Get Last Modified Date
-document.querySelector("#lastModified").innerHTML = document.lastModified;
