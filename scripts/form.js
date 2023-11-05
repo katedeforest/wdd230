2d186a1e59ae46aa1365fc6eb4fefae1827fc0d1
Kate DeForest <katedeforest02@gmail.com>
2023-11-04 21:46:05 -0600

    form build

--- scripts/form.js
+++ scripts/form.js
@@ -2,3 +2,15 @@ const rating = document.querySelector("#rating");
 rating.addEventListener("change", () => {
   document.querySelector("#current-rating").innerHTML = rating.value;
 });
+
+const password = document.querySelector("#password");
+const confirm = document.querySelector("#confirm");
+const message = document.querySelector("#message");
+confirm.addEventListener("blur", () => {
+  if (password.value != confirm.value) {
+    message.textContent = "Passwords do not match!";
+    password.focus();
+  } else {
+    message.textContent = "";
+  }
+});
