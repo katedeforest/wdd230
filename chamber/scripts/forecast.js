1b2e2c37e8f38a5154900ab7412579db8b21b632
Kate DeForest <katedeforest02@gmail.com>
2023-11-20 14:13:48 -0700

    chamber home weather

---
+++ chamber/scripts/forecast.js
@@ -0,0 +1,70 @@
+// Update this URL to your API call URL to openweather
+
+const forecastURL = "./data/forecast.json";
+const forecastLAT = 43.887093;
+const forecastLON = -111.6682194;
+const forecastAPIKEY = "f8beef3ef34344e18f60036585d585ad";
+// const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&appid=${APIKEY}&units=imperial`;
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
+function displayForecast(forecastData) {
+  // Get dates for next three days
+  console.log(forecastData);
+  let dates = [];
+  let mydate = new Date();
+  for (let i = 0; i < 3; i++) {
+    mydate = new Date(mydate.getTime() + ONE_DAY);
+    nextdate = mydate.toISOString().slice(0, 10);
+    dates.push(nextdate);
+  }
+  console.log(dates);
+
+  // Find the object with the highest temperature for each day
+  highTemps = dates.map((date) =>
+    forecastData
+      .filter((x) => x.dt_txt.startsWith(date))
+      .reduce((currentObj, highObj) =>
+        currentObj.main.temp > highObj.main.temp ? currentObj : highObj
+      )
+  );
+  // Find the object with the lowest temperature for each day
+  lowTemps = dates.map((date) =>
+    forecastData
+      .filter((x) => x.dt_txt.startsWith(date))
+      .reduce((currentObj, lowObj) =>
+        currentObj.main.temp < lowObj.main.temp ? currentObj : lowObj
+      )
+  );
+  console.log(highTemps);
+  console.log(lowTemps);
+
+  // Add the forecast information to the HTML document
+  weatherElt = document.querySelector("#forecast");
+  for (let i = 0; i < 3; i++) {
+    let newsection = document.createElement("section");
+    newsection.innerHTML = `<h2>${dates[i]}</h2><p>High: ${highTemps[
+      i
+    ].main.temp.toFixed(0)}&deg;</p><p>Low: ${lowTemps[i].main.temp.toFixed(
+      0
+    )}&deg;</p>`;
+    weatherElt.append(newsection);
+  }
+}
+
+async function getTheForecast() {
+  try {
+    const response = await fetch(weatherURL);
+    if (response.ok) {
+      const data = await response.json();
+      const listInfo = data.list;
+      displayForecast(listInfo);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getTheForecast();
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,20 +1,17 @@
-// I have intentionally set the weather data to a static weather file because I don't want to publish my API key.
-// If you borrow this example, you will need to get your own API KEY from OpenWeather as instructed
-// And use the commented out apiURL below.
+// WEATHER INFORMATION
 const LAT = "40.4968845";
 const LON = "-112.0710874";
 const APIKEY = "f4e7eabc96c8e72dd2d715d4dea5f961";
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}&units=imperial`;
-// const apiURL = "./data/weather.json";
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}&units=imperial`;
 
 function displayWeather(weatherData) {
   // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
   // const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
   const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
-  const temperature = weatherData.main.temp.toFixed(0);
+  const weatherTemp = weatherData.main.temp.toFixed(0);
   const desc = weatherData.weather[0].description;
   const humidity = weatherData.main.humidity.toFixed(0);
-  const windSpeed = weatherData.wind.speed.toFixed(0);
+  const weatherWindSpeed = weatherData.wind.speed.toFixed(0);
 
   //Set up the weather icon
   let skyIcon = document.querySelector("#sky-icon");
@@ -22,8 +19,7 @@ function displayWeather(weatherData) {
   skyIcon.setAttribute("alt", desc);
 
   //Set up the tempurature
-  let weatherTemp = document.querySelector("#temp");
-  weatherTemp.innerHTML = `${temperature}`;
+  tempuratureSpan.innerHTML = `${weatherTemp}`;
 
   //Set up the weather description
   let weatherDesc = document.querySelector("#description");
@@ -34,13 +30,12 @@ function displayWeather(weatherData) {
   weatherHum.innerHTML = `${humidity}`;
 
   //Set up the wind speed
-  let weatherWindSpeed = document.querySelector("#speed");
-  weatherWindSpeed.innerHTML = `${windSpeed}`;
+  windspeedSpan.innerHTML = `${weatherWindSpeed}`;
 }
 
 async function getTheWeather() {
   try {
-    const response = await fetch(apiURL);
+    const response = await fetch(weatherURL);
     if (response.ok) {
       const data = await response.json();
       displayWeather(data);
@@ -54,9 +49,9 @@ async function getTheWeather() {
 
 getTheWeather();
 
-//WIND CHILL
+// WIND CHILL CALCULATION
 function showWindChill(temp, speed) {
-  const windChillSpan = document.querySelector(".chill");
+  const windChillSpan = document.querySelector("#chill");
   let message = "N/A";
 
   if (temp <= 50 && speed > 3) {
